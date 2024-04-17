a32302afe8b764c0722c2cbf6fced8765be3dc84
Prahlad Ezequiel Tkach <prahladezequieltkach@gmail.com>
2024-04-16 20:20:03 -0400

    te dice el dia actual

--- asker.c
+++ asker.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <locale.h>
 
 int main()
 {
+	time_t tiempoActual;
+	struct tm *tiempoLocal;
+	char nombreDia[10];
+
+	setlocale(LC_TIME, "spanish.utf8");
+
+	tiempoActual = time(NULL);
+	tiempoLocal = localtime(&tiempoActual);
+	strftime(nombreDia, 10, "%A", tiempoLocal);
+
 	char text[20];
 	printf("que dia es hoy\n");
 	scanf("%s",text);
-	printf("hoy no es %s, hoy es martes\n",text);
+	if(strcmp(text, nombreDia) == 0)
+	{
+		printf("Si, Hoy es %s \n",text);
+	}
+	else
+	{
+		printf("No hoy no es %s, Hoy es %s\n",text);
+	}
+	return 0;
 }
